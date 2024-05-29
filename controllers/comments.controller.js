d0e575062cf8c3f0449ade21a2f47f72ac5f829b
ShubhamAgarwal-11 <shubham2002jindal@gmail.com>
2024-05-29 14:39:20 +0530

    create comments routes

---
+++ controllers/comments.controller.js
@@ -0,0 +1,37 @@
+const Comment = require('../models/comment.model')
+const Post = require('../models/post.model')
+exports.createComments = async(req,res)=>{
+    try {
+        const {body,user,post} = req.body;
+    
+        const commentObj = new Comment({
+            body,user
+        });
+        const savedComment = await commentObj.save();
+        const updatedPost = await Post.findByIdAndUpdate({_id : post}, {$push : {comments : savedComment._id}} , {new : true})
+        res.status(201).json({
+            success : true,
+            updatedPost : updatedPost
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success : true,
+            error : error.message
+        })
+    }
+}
+
+exports.getAllComments = async(req,res) =>{
+    try {
+        const allComments = await Comment.find({});
+        res.json({
+            success : true,
+            comments : allComments
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            error : error.message
+        })
+    }
+}
--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/post.controller')
+const commentController = require('../controllers/comments.controller')
 
 router.get('/posts',postController.getAllPosts)
 router.post('/posts/create',postController.createPost)
+
+router.get('/comments',commentController.getAllComments)
+router.post('/comments/create',commentController.createComments)
 
 module.exports = router;
